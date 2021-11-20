1e8b0d8a817b579eec153f9773966c81f0b76829
Abdul2067 <abdul2067@gmail.com>
2021-11-20 16:41:11 -0500

    add css styling and images

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+
+class Musician:
+  def __init__ (self, name, genre, description, age):
+    self.name = name
+    self.genre = genre
+    self.description = description
+    self.age = age
+
+musicians = [
+  Musician("Drake", "Hip-Hop", "Awesome Artist", "30"),
+  Musician("Drake", "Hip-Hop", "Awesome Artist", "30"),
+  Musician("Drake", "Hip-Hop", "Awesome Artist", "30"),
+  Musician("Drake", "Hip-Hop", "Awesome Artist", "30")
+]
 
 # Create your views here.
 
 def home(request):
-  return render(request, "/home")
+  return render(request, "home.html")
 
 def about(request):
   return render(request, "about.html")
+
+def musicians_index(request):
+  return render(request, "musicians/index.html", { "musicians" : musicians})
